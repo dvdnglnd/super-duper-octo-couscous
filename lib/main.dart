569e4e608bac7e2f29a31d8914fde46028b8bbf0
David England <dvdnglnd@gmail.com>
2021-04-24 10:39:34 +0700

    Fixed screen to portrait mode.

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:audioplayers/audio_cache.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 
 void main() => runApp(MyApp());
@@ -15,6 +16,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
         title: 'เสียงพระเจ้า',
         home: Scaffold(
